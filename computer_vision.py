afd24e8d947585b8f28e3df5ecc89ef7cb65da30
ortal1602 <47923357+ortal1602@users.noreply.github.com>
2020-04-16 21:10:32 +0300

    init

---
+++ computer_vision.py
@@ -0,0 +1,144 @@
+import numpy as np
+from scipy.ndimage.morphology import generate_binary_structure
+from scipy.ndimage import label, center_of_mass, map_coordinates
+from scipy.ndimage.filters import convolve, maximum_filter
+import cv2
+
+
+def warp_image(image, homography):
+    """
+    Warps an RGB image with a given homography.
+    :param image: an RGB image.
+    :param homography: homograhpy.
+    :return: A warped image.
+    """
+    return np.dstack([warp_channel(image[..., channel], homography) for channel in range(3)])
+
+
+def warp_channel(image: np.ndarray, homography: np.ndarray):
+    """
+    Warps a 2D image with a given homography.
+    :param image: a 2D image.
+    :param homography: homograhpy.
+    :return: A 2d warped image.
+    """
+    assert homography.shape == (3, 3)
+    h, w = image.shape
+    bbox = compute_bounding_box(homography, w, h)
+    x, y = np.meshgrid(range(bbox[0, 0], bbox[1, 0] + 1, 1),
+                       range(bbox[0, 1], bbox[1, 1] + 1, 1))
+    coords = np.vstack([x.flatten(), y.flatten()]).transpose()
+    H_inv = np.linalg.inv(homography)
+    org_coords = np.fliplr(apply_homography(coords, H_inv))
+    canvas = map_coordinates(image, org_coords.transpose(), order=1, prefilter=True)
+    res = canvas.reshape(x.shape)
+    return res
+
+
+def apply_homography(pos1, H12):
+    """
+    Apply homography to inhomogenous points.
+    :param pos1: An array with shape (N,2) of [x,y] point coordinates.
+    :param H12: A 3x3 homography matrix.
+    :return: An array with the same shape as pos1 with [x,y] point coordinates obtained from transforming pos1 using H12.
+    """
+    pts = np.stack((pos1[:, 0], pos1[:, 1], np.ones((pos1.shape[0]), dtype=np.int)), axis=-1)
+    ret = np.transpose(np.dot(H12, np.transpose(pts)))
+    ret = np.true_divide(ret[:, : 2], ret[:, [-1]])
+    return ret
+
+
+def compute_bounding_box(homography, w, h):
+    """
+    computes bounding box of warped image under homography, without actually warping the image
+    :param homography: homography
+    :param w: width of the image
+    :param h: height of the image
+    :return: 2x2 array, where the first row is [x,y] of the top left corner,
+     and the second row is the [x,y] of the bottom right corner
+    """
+    corners = apply_homography(np.asarray([[0, 0], [w - 1, 0], [0, h - 1], [w - 1, h - 1]]),
+                               homography)
+    top_left = np.asarray([np.amin(corners[:, 0]), np.amin(corners[:, 1])])
+    bottom_right = np.asarray([np.amax(corners[:, 0]), np.amax(corners[:, 1])])
+    res = np.asarray([top_left, bottom_right]).astype(np.int)
+    return res
+
+
+# ---- Rigid transformations & estimation -----
+def estimate_rigid_transform(points1, points2, translation_only=False):
+    """
+    Computes rigid transforming points1 towards points2, using least squares method.
+    points1[i,:] corresponds to poins2[i,:]. In every point, the first coordinate is *x*.
+    :param points1: array with shape (N,2). Holds coordinates of corresponding points from image 1.
+    :param points2: array with shape (N,2). Holds coordinates of corresponding points from image 2.
+    :param translation_only: whether to compute translation only. False (default) to compute rotation as well.
+    :return: A 3x3 array with the computed homography.
+    """
+    centroid1 = points1.mean(axis=0)
+    centroid2 = points2.mean(axis=0)
+
+    if translation_only:
+        rotation = np.eye(2)
+        translation = centroid2 - centroid1
+
+    else:
+        centered_points1 = points1 - centroid1
+        centered_points2 = points2 - centroid2
+
+        sigma = centered_points2.T @ centered_points1
+        U, _, Vt = np.linalg.svd(sigma)
+
+        rotation = U @ Vt
+        translation = -rotation @ centroid1 + centroid2
+
+    H = np.eye(3)
+    H[:2, :2] = rotation
+    H[:2, 2] = translation
+    return H
+
+
+def non_maximum_suppression(image):
+    """
+    Finds local maximas of an image.
+    :param image: A 2D array representing an image.
+    :return: A boolean array with the same shape as the input image, where True indicates local maximum.
+    """
+    # Find local maximas.
+    neighborhood = generate_binary_structure(2, 2)
+    local_max = maximum_filter(image, footprint=neighborhood) == image
+    local_max[image < (image.max() * 0.1)] = False
+
+    # Erode areas to single points.
+    lbs, num = label(local_max)
+    centers = center_of_mass(local_max, lbs, np.arange(num) + 1)
+    centers = np.stack(centers).round().astype(np.int)
+    ret = np.zeros_like(image, dtype=np.bool)
+    ret[centers[:, 0], centers[:, 1]] = True
+    return ret
+
+
+def spread_out_corners(im, m, n, radius):
+    """
+    Splits the image im to m by n rectangles and uses harris_corner_detector on each.
+    :param im: A 2D array representing an image.
+    :param m: Vertical number of rectangles.
+    :param n: Horizontal number of rectangles.
+    :param radius: Minimal distance of corner points from the boundary of the image.
+    :return: An array with shape (N,2), where ret[i,:] are the [x,y] coordinates of the ith corner points.
+    """
+    corners = [np.empty((0, 2), dtype=np.int)]
+    x_bound = np.linspace(0, im.shape[1], n + 1, dtype=np.int)
+    y_bound = np.linspace(0, im.shape[0], m + 1, dtype=np.int)
+    for i in range(n):
+        for j in range(m):
+            # Use Harris detector on every sub image.
+            sub_im = im[y_bound[j]:y_bound[j + 1], x_bound[i]:x_bound[i + 1]]
+            sub_corners = cv2.cornerHarris(sub_im, 2, 3, 0.04)
+            sub_corners += np.array([x_bound[i], y_bound[j]])[np.newaxis, :]
+            corners.append(sub_corners)
+    corners = np.vstack(corners)
+    legit = ((corners[:, 0] > radius) & (corners[:, 0] < im.shape[1] - radius) &
+             (corners[:, 1] > radius) & (corners[:, 1] < im.shape[0] - radius))
+    ret = corners[legit, :]
+    return ret
---
+++ drawing_functions.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+
+"""
+===============================================================================
+====================   Functions and general description    ====================
+===============================================================================
+
+mark_points(n: int, img):
+    marks n points on image (or less if 'q' was pressed)
+    :param img: image to mark pts on
+    :param n: bound num
+    :dependencies: none
+"""
+
+
+def mark_points(n: int, img):
+    """
+    marks n points on image (or less if 'q' was pressed)
+    :param img: image to mark pts on
+    :param n: bound num
+    """
+    def get_points(event, x, y, flags, params):
+        global arr, check
+        if event == cv2.EVENT_LBUTTONDOWN:
+            arr.append([x, y])
+            check += 1
+        elif event == cv2.EVENT_LBUTTONUP:
+            cv2.circle(img, (x, y), 4, (0, 255, 0), -1)
+            cv2.imshow("image", img)
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", get_points)
+    check = 0
+    while True:
+        cv2.imshow("image", img)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+        elif check == n:
+            break
